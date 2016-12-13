08816092713624e4ff2630fe87ff0a2fedb49b55
Claudio Sampaio <claudio.sampaio@conatussw.com>
2016-12-13 15:04:14 -0400

    Export do pequeno cacique.

---
+++ offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1481654988,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"images/player-sheet0.png",
+		"images/tilemap.png",
+		"images/bow-sheet0.png",
+		"images/arrow-sheet0.png",
+		"images/rock-sheet0.png",
+		"images/playerphysics-sheet0.png",
+		"images/rockbase-sheet0.png",
+		"images/arrawcount.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
